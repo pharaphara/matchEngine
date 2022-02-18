393d788b27d825493256478e914c7ade52ae78e6
alex <alexander.deltour@gmail.com>
2022-02-18 12:26:45 +0100

    cors

--- src/main/java/fr/eql/eqlXchange/matchEngine/Controller/PriceController.java
+++ src/main/java/fr/eql/eqlXchange/matchEngine/Controller/PriceController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import fr.eql.eqlXchange.matchEngine.services.serviceInterface.PriceServices;
 
 @RestController
 @RequestMapping("/price")
+@CrossOrigin(origins = "*")
 public class PriceController {
 
 	@Autowired
